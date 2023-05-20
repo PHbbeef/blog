5951a4fa983d5c84e75e60d230c40b052c5e9783
PHb <tconcave9@gmail.com>
2023-05-21 00:25:25 +0800

    增删文件

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace WebApi.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "你好世界！";
+        }
+
+        [HttpGet]
+        public string Getname(string name)
+        {
+            return "你好" + name;
+        }
+
+        [HttpGet]
+        public string Login(string user, string password)
+        {
+            if (user == "admin" && password == "20050211")
+            {
+                return "登录成功！";
+            }
+            else
+            {
+                return "密码或账户错误";
+            }
+        }
+    }
+}
